c773f8e748fbd38cb16d2d3fc21315274c68268e
Muhamad Krisnandi <mkrisnandi08@gmail.com>
2022-12-20 11:11:38 +0700

    add send email after checkout & after paid

--- app/Http/Controllers/Admin/CheckoutController.php
+++ app/Http/Controllers/Admin/CheckoutController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Checkout;
 use Illuminate\Http\Request;
+use App\Mail\Checkout\Paid;
+use Illuminate\Support\Facades\Mail;
 
 
 class CheckoutController extends Controller
@@ -13,6 +15,9 @@ class CheckoutController extends Controller
     {
         $checkout->is_paid = true;
         $checkout->save();
+
+         // send email to user
+         Mail::to($checkout->User->email)->send(new Paid($checkout));
 
         $request->session()->flash('success', "Checkout with ID {$checkout->id} has been updated!");
         return redirect(route('admin.dashboard'));
